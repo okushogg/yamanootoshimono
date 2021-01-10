8318c9ab7193ed112ec7da3757a155413a938ed3
okushogg <okusho721@gmail.com>
2021-01-10 02:00:58 +0000

    User機能を追加

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get users_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get users_edit_url
+    assert_response :success
+  end
+
+  test "should get unsubscribe" do
+    get users_unsubscribe_url
+    assert_response :success
+  end
+
+end
