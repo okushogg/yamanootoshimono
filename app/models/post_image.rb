291200133e79c17967278a4775fa42266f5db3f8
okushogg <okusho721@gmail.com>
2021-01-18 00:44:37 +0000

    各Modelのリレーション追加。

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,2 +1,4 @@
 class PostImage < ApplicationRecord
+  belongs_to :post, :dependent => :destroy
+  attachment :image
 end
--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -1,2 +1,4 @@
 class Prefecture < ApplicationRecord
+  has_many :posts
+  has_many :places
 end
