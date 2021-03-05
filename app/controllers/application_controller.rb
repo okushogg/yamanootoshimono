5bb5a8a0481d9402b1da9d445f962d4505049352
okushogg <okusho721@gmail.com>
2021-03-05 05:42:02 +0000

    Application_controller例外処理部分変更。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,11 +10,11 @@ end
 
 
 def render_404
-  render 'error/404', status: :not_found
+  render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
 end
 
 def render_500
-  render 'error/500', status: :internal_server_error
+  render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
 end
 
   protected
