f5785e7ea09b4c9d0ac9b18f95b6879bc110e213
okushogg <okusho721@gmail.com>
2021-03-21 06:24:13 +0000

    Item_genreリファクタリング完了

--- app/helpers/item_genres_helper.rb
+++ app/helpers/item_genres_helper.rb
@@ -1,2 +1,12 @@
 module ItemGenresHelper
+  def get_item_genre_path(item_genre_id, path_name)
+    case path_name
+      when "item_genre" then
+        item_genre_path(item_genre_id)
+      when "pick_up" then
+        step4_path(item_genre_id)
+      when "drops" then
+        phase4_path(item_genre_id)
+    end
+  end
 end
